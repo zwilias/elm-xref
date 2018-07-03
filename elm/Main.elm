5af5019b147ea18206f0bc6d7ad906cb4802ffc5
ilias <mail@ilias.xyz>
2018-07-03 22:17:24 +0200

    Initial import

---
+++ elm/Main.elm
@@ -0,0 +1,260 @@
+port module Main exposing (main, toElm, toJS)
+
+import Dict exposing (Dict)
+import Elm.Interface as Interface exposing (Interface)
+import Elm.Parser as Parser
+import Elm.Processing as Processing
+import Elm.RawFile as RawFile exposing (RawFile)
+import Elm.Syntax.Base as Syntax
+import Elm.Syntax.Declaration exposing (Declaration)
+import Elm.Syntax.Module exposing (Import)
+import Elm.Syntax.Ranged exposing (Ranged)
+import Json.Encode as Json
+import Set exposing (Set)
+
+
+type alias Model =
+    { todo :
+        List
+            { blockedBy : Set Syntax.ModuleName
+            , data : Module
+            }
+    , packageData : Dict Syntax.ModuleName InterfaceData
+    , done : Dict Syntax.ModuleName Module
+    }
+
+
+type alias InFile =
+    { package : Maybe PackageIdentifier
+    , fileName : String
+    , content : String
+    }
+
+
+type alias PackageIdentifier =
+    { name : String
+    , version : String
+    }
+
+
+type Msg
+    = Parse InFile
+    | Send
+
+
+init : ( Model, Cmd msg )
+init =
+    ( { todo = []
+      , packageData = Dict.empty
+      , done = Dict.empty
+      }
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Parse file ->
+            case parse file of
+                Ok mod ->
+                    ( add mod model
+                    , toJS <| Json.string <| "parsed " ++ file.fileName
+                    )
+
+                Err e ->
+                    ( model
+                    , toJS <| Json.string <| file.fileName ++ ": " ++ toString e
+                    )
+
+        Send ->
+            ( model, toJS <| todoToJson model.todo )
+
+
+todoToJson : List { blockedBy : Set Syntax.ModuleName, data : Module } -> Json.Value
+todoToJson todo =
+    List.map
+        (\{ blockedBy, data } ->
+            Json.object
+                [ ( "module"
+                  , Json.string <| String.join "." data.name
+                  )
+                , ( "blockedBy"
+                  , blockedBy
+                        |> Set.toList
+                        |> List.map (String.join "." >> Json.string)
+                        |> Json.list
+                  )
+                ]
+        )
+        todo
+        |> Json.list
+
+
+type alias Errors =
+    List String
+
+
+add : InModule -> Model -> Model
+add mod model =
+    case mod of
+        Package package ->
+            { model | packageData = Dict.insert package.name package model.packageData }
+                |> checkTodo (Set.singleton package.name)
+
+        Own modul ->
+            let
+                blockers =
+                    requiredModules modul model
+            in
+            if Set.isEmpty blockers then
+                { model | done = Dict.insert modul.name modul model.done }
+                    |> checkTodo (Set.singleton modul.name)
+            else
+                { model | todo = { blockedBy = blockers, data = modul } :: model.todo }
+
+
+checkTodo : Set Syntax.ModuleName -> Model -> Model
+checkTodo nowDone model =
+    let
+        ( newlyDone, newTodo ) =
+            List.foldl
+                (\{ blockedBy, data } ( done, todo ) ->
+                    let
+                        newBlockers =
+                            Set.diff blockedBy nowDone
+                    in
+                    if Set.isEmpty newBlockers then
+                        ( data :: done, todo )
+                    else
+                        ( done, { blockedBy = newBlockers, data = data } :: todo )
+                )
+                ( [], [] )
+                model.todo
+
+        newDone =
+            List.map (\m -> ( m.name, m )) newlyDone |> Dict.fromList
+
+        newModel =
+            { model | todo = newTodo, done = Dict.union model.done newDone }
+    in
+    case newlyDone of
+        [] ->
+            newModel
+
+        _ ->
+            checkTodo (Dict.keys newDone |> Set.fromList) newModel
+
+
+requiredModules : Module -> Model -> Set Syntax.ModuleName
+requiredModules modul model =
+    let
+        needed =
+            List.map .moduleName modul.imports
+                |> List.filter (not << isNative)
+                |> Set.fromList
+    in
+    Set.diff needed (availableModules model)
+
+
+isNative : Syntax.ModuleName -> Bool
+isNative =
+    List.head >> Maybe.map ((==) "Native") >> Maybe.withDefault False
+
+
+availableModules : Model -> Set Syntax.ModuleName
+availableModules model =
+    List.concat
+        [ Dict.keys model.packageData
+        , Dict.keys model.done
+        ]
+        |> Set.fromList
+
+
+parse : InFile -> Result Errors InModule
+parse file =
+    Parser.parse file.content
+        |> Result.map (toModule file.package file.fileName)
+
+
+toModule : Maybe PackageIdentifier -> String -> RawFile -> InModule
+toModule package fileName rawFile =
+    case package of
+        Just packageInfo ->
+            toPackageModule packageInfo fileName rawFile
+
+        Nothing ->
+            toInternalModule fileName rawFile
+
+
+toPackageModule : PackageIdentifier -> String -> RawFile -> InModule
+toPackageModule package fileName rawFile =
+    Package
+        { name = RawFile.moduleName rawFile |> Maybe.withDefault []
+        , fileName = fileName
+        , interface = Interface.build rawFile
+        , package = package
+        }
+
+
+toInternalModule : String -> RawFile -> InModule
+toInternalModule fileName rawFile =
+    let
+        file =
+            Processing.process Processing.init rawFile
+    in
+    Own
+        { name = RawFile.moduleName rawFile |> Maybe.withDefault []
+        , fileName = fileName
+        , interface = Interface.build rawFile
+        , imports = file.imports
+        , declarations = file.declarations
+        }
+
+
+type InModule
+    = Package InterfaceData
+    | Own Module
+
+
+type alias InterfaceData =
+    { name : Syntax.ModuleName
+    , fileName : String
+    , interface : Interface
+    , package : PackageIdentifier
+    }
+
+
+type alias Module =
+    { name : Syntax.ModuleName
+    , fileName : String
+    , interface : Interface
+    , imports : List Import
+    , declarations : List (Ranged Declaration)
+    }
+
+
+port toElm : (InFile -> msg) -> Sub msg
+
+
+port toJS : Json.Value -> Cmd msg
+
+
+port fetch : (() -> msg) -> Sub msg
+
+
+subscriptions : Sub Msg
+subscriptions =
+    Sub.batch
+        [ toElm Parse
+        , fetch (always Send)
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Platform.program
+        { init = init
+        , update = update
+        , subscriptions = always subscriptions
+        }
---
+++ index.js
@@ -0,0 +1,167 @@
+#!/usr/bin/env node
+
+var Elm = require("./elm.js");
+var Promise = require("bluebird");
+var fs = require("fs-extra");
+var klaw = require("klaw");
+var through2 = require("through2");
+
+var app = Elm.Main.worker();
+
+app.ports.toJS.subscribe(console.dir);
+
+fs
+    .readFile("elm-package.json")
+    .then(data => JSON.parse(data))
+    .then(info =>
+        Promise.map(Object.keys(info.dependencies), parsePackage).then(
+            () => info
+        )
+    )
+    .then(info => Promise.map(info["source-directories"], parseSources))
+    .then(() => app.ports.fetch.send(null));
+
+function parsePackage(packageName) {
+    return parsePackageName(packageName)
+        .then(findVersion)
+        .then(findExposedModules)
+        .then(pkg => Promise.map(pkg.modules, parsePackageModule(pkg)));
+}
+
+function parseSources(sourceDirectory) {
+    return findElmFiles(sourceDirectory).then(sources =>
+        Promise.map(sources, parseSource)
+    );
+}
+
+function parseSource(modulePath) {
+    return fs.readFile(modulePath).then(data =>
+        app.ports.toElm.send({
+            fileName: modulePath,
+            content: data.toString(),
+            package: null
+        })
+    );
+}
+
+var excludeDirFilter = through2.obj(function(item, enc, next) {
+    if (!item.stats.isDirectory()) {
+        this.push(item);
+    }
+    next();
+});
+
+var excludeElmStuffFilter = through2.obj(function(item, enc, next) {
+    if (!item.path.includes("elm-stuff")) {
+        this.push(item);
+    }
+    next();
+});
+
+var excludeTestsFilter = through2.obj(function(item, enc, next) {
+    if (!item.path.includes("tests")) {
+        this.push(item);
+    }
+    next();
+});
+
+var onlyElmFilesFilter = through2.obj(function(item, enc, next) {
+    if (item.path.endsWith(".elm")) {
+        this.push(item);
+    }
+    next();
+});
+
+function findElmFiles(sourceDirectory) {
+    return new Promise(function(resolve, reject) {
+        var files = [];
+
+        klaw(sourceDirectory)
+            .pipe(excludeDirFilter)
+            .pipe(excludeElmStuffFilter)
+            .pipe(excludeTestsFilter)
+            .pipe(onlyElmFilesFilter)
+            .on("data", fileName => files.push(fileName.path))
+            .on("end", () => resolve(files));
+    });
+}
+
+function parsePackageModule(pkg) {
+    var pkgName = packageToName(pkg);
+    var pkgPath = basePath(pkg);
+
+    return function(moduleName) {
+        var fileNames = [
+            pkgPath + "src/" + modulePath(moduleName),
+            pkgPath + modulePath(moduleName)
+        ];
+
+        return Promise.any(
+            fileNames.map(name =>
+                fs.readFile(name).then(content =>
+                    app.ports.toElm.send({
+                        package: {
+                            name: packageToName(pkg),
+                            version: pkg.version
+                        },
+                        fileName: name,
+                        content: content.toString()
+                    })
+                )
+            )
+        );
+    };
+}
+
+function modulePath(name) {
+    return name.split(".").join("/") + ".elm";
+}
+
+function parsePackageName(name) {
+    var parts = name.split("/");
+    if (parts.length == 2) {
+        return Promise.resolve({ author: parts[0], name: parts[1] });
+    } else {
+        return Promise.reject("failed to parse " + name);
+    }
+}
+
+function findVersion(pkg) {
+    var versionsPath = "elm-stuff/packages/" + pkg.author + "/" + pkg.name;
+
+    return fs
+        .readdir(versionsPath)
+        .then(data => data.filter(v => !(v == "." || v == "..")))
+        .then(versions => {
+            if (versions.length !== 1) {
+                return Promise.reject(
+                    "Found multiple candidate versions for " +
+                        packageToName(pkg)
+                );
+            } else {
+                return { ...pkg, version: versions[0] };
+            }
+        });
+}
+function basePath(pkg) {
+    return (
+        "elm-stuff/packages/" +
+        pkg.author +
+        "/" +
+        pkg.name +
+        "/" +
+        pkg.version +
+        "/"
+    );
+}
+
+function findExposedModules(pkg) {
+    return fs.readFile(basePath(pkg) + "elm-package.json").then(data => {
+        var info = JSON.parse(data);
+        return { ...pkg, modules: info["exposed-modules"] };
+    });
+}
+
+function packageToName(package) {
+    return package.author + "/" + package.name;
+}
