22e26eb4324f0b131724e2a8c7faac24f993aa67
ilias <mail@ilias.xyz>
2019-02-20 23:34:57 +0100

    I did something with the tests

--- tests/basic.js
+++ tests/basic.js
@@ -2,26 +2,38 @@ var t = require("tap"),
     runner = require("../lib/runner.js");
 
 t.test("Entry points are used", function(t) {
-    return parseContent(`
+    return initParser()
+        .then(
+            parseContent(`
 module Main exposing (main)
 main = "hi there!"
-`).then(function(unused) {
-        t.same(unused, []);
-    });
+`)
+        )
+        .then(runner.findUnused)
+        .then(function(unused) {
+            t.same(unused, []);
+        });
 });
 
 t.test("Unused function is unused", function(t) {
-    return parseContent(`
+    return initParser()
+        .then(
+            parseContent(`
 module Main exposing (main)
 unused = "not used!"
 main = "this is used"
-`).then(function(unused) {
-        t.notSame(unused, []);
-    });
+`)
+        )
+        .then(runner.findUnused)
+        .then(function(unused) {
+            t.notSame(unused, []);
+        });
 });
 
 t.test("Constructor only used in self match is unused", function(t) {
-    return parseContent(`
+    return initParser()
+        .then(
+            parseContent(`
 module Main exposing (main)
 
 type Foo = Foo | Bar
@@ -33,23 +45,59 @@ map f =
     Bar -> Bar
 
 main = map Foo
-`).then(function(unused) {
-        t.notSame(unused, []);
-    });
+`)
+        )
+        .then(runner.findUnused)
+        .then(function(unused) {
+            t.notSame(unused, []);
+        });
 });
 
-function parseContent(content) {
-    var app = runner.initElm(["Main"]);
+t.test("Let declaration shadows imported module", function(t) {
+    return initParser()
+        .then(
+            parseContent(
+                `
+module ToImport exposing (importedFn)
+
+importedFn : String
+importedFn = "hello"
+`,
+                "ToImport.elm"
+            )
+        )
+        .then(
+            parseContent(`
+module Importer exposing (main)
+
+import ToImport exposing (importedFn)
+
+main =
+  let importedFn = "shadow" in importedFn
+`)
+        )
+        .then(runner.findUnused)
+        .then(function(unused) {
+            t.notSame(unused, []);
+        });
+});
+
+function initParser() {
+    var app = runner.initElm();
 
     return runner
         .parseAppPackage(app)(["elm/core", "1.0.0"])
-        .then(function() {
-            app.ports.toElm.send({
-                fileName: "Foo.elm",
-                content: content,
-                package: null
-            });
-            return app;
-        })
-        .then(runner.findUnused);
+        .then(() => app);
+}
+
+function parseContent(content, fileName = "Foo.elm") {
+    return function(app) {
+        app.ports.toElm.send({
+            fileName,
+            content,
+            package: null
+        });
+
+        return app;
+    };
 }
